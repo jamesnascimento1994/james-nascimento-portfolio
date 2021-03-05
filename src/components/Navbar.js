9db64e81b22dc22ee0938b975029ef905b6b9238
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-03-05 17:40:50 -0500

    added functionality for fa fa bars, made navbar more responsive, installed bootstrap fontawesome packages

---
+++ src/components/Navbar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+// REACT FONT AWESOME INPUT
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = () => {
+	return (
+		<nav className='navbar navbar-expand-lg navbar-light bg-dark'>
+			<div className='container'>
+				<a className='navbar-brand' href='#'>
+					<h3 className='header'>James Nascimento</h3>
+				</a>
+				<button
+					className='navbar-toggler'
+					type='button'
+					data-toggle='collapse'
+					data-target='#navbarSupportedContent'
+					aria-controls='navbarSupportedContent'
+					aria-expanded='false'
+					aria-label='Toggle navigation'>
+					<FontAwesomeIcon icon={faBars} style={{ color: "#fff" }}/>
+				</button>
+
+				<div className='collapse navbar-collapse' id='navbarSupportedContent'>
+					<ul className='navbar-nav ml-auto'>
+						<li className='nav-item active'>
+							<a className='nav-link' href='#'>
+								Home <span className='sr-only'>(current)</span>
+							</a>
+						</li>
+						<li className='nav-item'>
+							<a className='nav-link' href='#'>
+								About Me
+							</a>
+						</li>
+						<li className='nav-item'>
+							<a className='nav-link' href='#'>
+								Skills
+							</a>
+						</li>
+						<li className='nav-item'>
+							<a className='nav-link' href='#'>
+								Portfolio
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
