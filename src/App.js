b87680c1d446372217a57220515d34f725b4fc9f
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-03-07 20:13:52 -0500

    Header complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Particles from 'react-particles-js';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
-    <Navbar />
-  );
+		<>
+			<Particles
+      className="particles-canvas"
+				params={{
+					particles: {
+						number: {
+							value: 20,
+							density: {
+								enable: true,
+								value_area: 900,
+							},
+						},
+						shape: {
+							type: 'circle',
+							stroke: {
+								width: 6,
+								color: '#c6c6c6',
+							},
+						},
+					},
+				}}
+			/>
+			<Navbar />
+			<Header />
+		</>
+	);
 }
 
 export default App;
