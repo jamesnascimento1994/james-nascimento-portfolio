d7f2e2de10b0d177a4e665fc0f68e678c5b76fe8
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-03-24 15:51:42 -0400

    added about text

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,11 @@ const About = () => {
 			<div className='col-lg-6 col-xm-12'>
                 <h1 className="about-heading">About Me</h1>
                 <p>
-                Paragraphs
+                I am a software engineer located in the East Coast. The applications I enjoy making are friendly to the user and I think that is super important especially since our current social climate is rather unfriendly.
+                </p>
+                <br/>
+                <p>
+                When challenges arise and I am under pressure, I remain calm, positive, and approach each issue with a growth mindset. This maximizes my productivity and efficiency.
                 </p>
             </div>
 				</div>
